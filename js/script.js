133e330fdec854cada1d04e174effc5e3839db58
Mikhail <mickshw@yandex.ru>
2022-02-13 13:03:05 +0300

    вынес файлы бутстрапа локально, мелкие правки

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-let exampleModal = document.getElementById("exampleModal");
+let exampleModal = document.querySelector("#exampleModal");
 exampleModal.addEventListener("show.bs.modal", function (event) {
 	let button = event.relatedTarget;
 	let value = button.getAttribute("data-bs-whatever");
 
 	let img_charters = exampleModal.querySelector("img.card-img-top");
-	img_charters.setAttribute("src", "images/build/" + value);
+	img_charters.setAttribute("src", "img/build/" + value);
 });
 
 // const data = [{
@@ -51,19 +51,19 @@ exampleModal.addEventListener("show.bs.modal", function (event) {
 // modalTitle.textContent = item.russia_name
 
 // let img_weapon = exampleModal.querySelector('img.weapon-img');
-// img_weapon.setAttribute("src", "images/" + item.weapon.name + ".png")
+// img_weapon.setAttribute("src", "img/" + item.weapon.name + ".png")
 //
 // let img_artefact_1 = exampleModal.querySelector('.artefact-img-1');
-// img_artefact_1.setAttribute("src", "images/" + item.artifact["1"].name + "_1.png")
+// img_artefact_1.setAttribute("src", "img/" + item.artifact["1"].name + "_1.png")
 //
 // let img_artefact_2 = exampleModal.querySelector('.artefact-img-2');
-// img_artefact_2.setAttribute("src", "images/" + item.artifact["2"].name + "_2.png")
+// img_artefact_2.setAttribute("src", "img/" + item.artifact["2"].name + "_2.png")
 //
 // let img_artefact_3 = exampleModal.querySelector('.artefact-img-3');
-// img_artefact_3.setAttribute("src", "images/" + item.artifact["3"].name + "_3.png")
+// img_artefact_3.setAttribute("src", "img/" + item.artifact["3"].name + "_3.png")
 //
 // let img_artefact_4 = exampleModal.querySelector('.artefact-img-4');
-// img_artefact_4.setAttribute("src", "images/" + item.artifact["4"].name + "_4.png")
+// img_artefact_4.setAttribute("src", "img/" + item.artifact["4"].name + "_4.png")
 //
 // let img_artefact_5 = exampleModal.querySelector('.artefact-img-5');
-// img_artefact_5.setAttribute("src", "images/" + item.artifact["5"].name + "_5.png")
+// img_artefact_5.setAttribute("src", "img/" + item.artifact["5"].name + "_5.png")
